5d5bb47f1094c04d4ff83aee425259b09d00718d
sakshi0303 <65711308+sakshi0303@users.noreply.github.com>
2024-08-20 16:11:43 +0530

    mongo download node js, Crud functionalty in mangodb

---
+++ node.js-backend/node.js
@@ -0,0 +1,37 @@
+// Node.js--> run time environment (when we run the code it will exexute)
+// node.js runs in terminal and  runs javascript outside the browser
+// connect with
+// it is not a programing language
+//
+// chrome has javasrict version v-8 enginer
+// firefox-> spidermonkey
+// // npm init -y ()
+// --npm i express mongodb
+// --package.json--> basic information
+// --go to mongodb website and go to products and click on Try COMMUNITY Edition
+
+
+download in brew
+// npm i express 
+// npm i -g nodemon (server keeps on running)
+
+// mongo db compass :
+//download mongodb compass:
+
+
+nodemon .\server.js
+
+mongodb::
+  - it is no sql database
+  -here data is stored in a form of Object
+  -the data stored in the collection
+  -collection do not have row and columns
+
+CRUD functionality in CRUD
+> show dbs   // show all database
+> use trail            // switched to db trail dataabase
+> db.createCollection('User')   // create collection  { ok: 1 }
+> db.User.insertOne({name:'sakshi',age:30})  // insert the value  acknowledged: true,
+> db.User.insertMany([{name:'sakshi',age:39},{name:'sakshi2',age:44}])
+
+it will automitically create unique id
