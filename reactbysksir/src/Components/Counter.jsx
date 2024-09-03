1379a45ced3596b31a4c561cd1c0b4643c80647f
sakshi0303 <65711308+sakshi0303@users.noreply.github.com>
2024-09-03 18:12:14 +0530

    usestate

--- reactbysksir/src/Components/Counter.jsx
+++ reactbysksir/src/Components/Counter.jsx
@@ -1,4 +1,3 @@
-let x = 10;
 //hook: reprint in the website with new values
 //     > useState hook const[variable,function]=useState(10)
 //> usestate ----render the page when function is called and replace the value is change and component rerender
@@ -13,17 +12,19 @@ let x = 10;
 //     > usestate hook is used to store the value and can also used to trigger rerender of component after a period of time
 //     > it takes two argument [value ,function] a value and a function
 
-import React, { useState } from "react";
+import React from 'react'
+import { useState } from 'react';
 
 const Counter = () => {
-  const [x, setx] = useState(10);
-  function increment() {
-    setx(x + 1);
-  }
 
-  const decrement = () => {
-    setx(x - 1);
-  };
+  const [x,setx]=useState(10);
+  function increment(){
+    //console.log("inside incremetn fn");
+    setx(x+1)
+  }
+  const decrement=()=>{
+    setx(x-1)
+  }
 
   const multiplyby2 = () => {
     setx(x * 2);
@@ -36,18 +37,18 @@ const Counter = () => {
   const devideby2 = () => {
     setx(x / 2);
   };
-
+  
   return (
     <div>
-      <h1>this is counter page</h1>
-      <p>value: {x}</p>
-      <button onlclick={increment}>Increment</button>
-      <button onclick={decrement}>Decrement</button>
-      <button onclick={multiplyby2}>multiplyby2</button>
-      <button onclick={devideby2}>devideby2</button>
-      <button onclick={multiplyby5}>multiplyby5</button>
+      <h1>this is Counter Page</h1>
+      <p>value x ={x}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={multiplyby2}>multiplyby2</button>
+      <button onClick={devideby2}>devideby2</button>
+      <button onClick={multiplyby5}>multiplyby5</button>
     </div>
-  );
-};
+  )
+}
 
-export default Counter;
+export default Counter
--- reactbysksir/src/index.js
+++ reactbysksir/src/index.js
@@ -6,9 +6,9 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  //<React.StrictMode> mutiple line console print
+  //<React.StrictMode> print all element in console
     <App />
-  
+  //</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
