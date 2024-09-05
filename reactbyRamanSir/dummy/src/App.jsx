5657c813b86b184c47f815331841ae10b1219d9e
sakshi0303 <65711308+sakshi0303@users.noreply.github.com>
2024-09-05 14:18:39 +0530

    dad

--- reactbyRamanSir/dummy/src/App.jsx
+++ reactbyRamanSir/dummy/src/App.jsx
@@ -3,6 +3,11 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Card from "./Card";
+import {Outlet}  from 'react-router-dom'
+import Navbar from "./Components/NavBar";
+import Footer from "./Components/Footer";
+
+//todolist
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,6 +17,12 @@ function App() {
       <h1 className="text-3xl font-bold underline">hellow world</h1>
       <Card name="Sakshi" paragraph=" Sakshi Descripting" link="Click here" />
       <Card name="Swarna" paragraph=" Swarnd Descripting" link="Click " />
+
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
+
+
     </>
   );
 }
--- reactbyRamanSir/dummy/src/main.jsx
+++ reactbyRamanSir/dummy/src/main.jsx
@@ -3,8 +3,27 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import {createBrowserRouter,createRoutesFromElements,Route,RouterProvider} from 'react-router-dom'
+import Home from './Components/Home.jsx'
+import AboutUs from './Components/AboutUs.jsx'
+import Contact from './Components/Contact.jsx'
+
+
+
+let router=createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='' element={<App/>}>
+      <Route path='' element={<Home/>}/>
+      <Route path='aboutus' element={<AboutUs/>}/>
+      <Route path='contact' element={<Contact/>}/> 
+
+    </Route>
+
+  )
+)
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
