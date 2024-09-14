970f47903211277049d43c41be22791adb87773b
sakshi0303 <65711308+sakshi0303@users.noreply.github.com>
2024-09-14 12:59:34 +0530

    object.entries map([key,value]

--- reactbysksir/src/Pages/ViewReceipe.jsx
+++ reactbysksir/src/Pages/ViewReceipe.jsx
@@ -5,7 +5,11 @@ const ViewReceipe = () => {
     let location=useLocation()
     console.log(location.state);
     let recipe=location.state.recipe
+    let totalNutrientsarr = Object.entries(recipe.totalNutrients)
+    //console.log(totalNutrientsarr)
 
+    let totalDailyarr=Object.entries(recipe.totalDaily)
+    console.log(totalDailyarr);
    
   return (
       <div className="container">
@@ -28,27 +32,37 @@ const ViewReceipe = () => {
           <div className="col-md-6">
             <h3>Ingrediant Lines</h3>
             <ul>
-            {recipe.ingredientLines.map((ele)=>{
-              return <li> {ele}</li>
+            {recipe.ingredientLines.map((ele,i)=>{
+              return <li key={i}> {ele}</li>
             })}
             </ul>
           </div>
-          {/* <div className="col">
+          <div className="col">
             <ul>
+              <h1>Total Nutrients</h1>
             {
-            recipe.totalNutrients.map((ele)=>{
-              return <li> label:{ele.label} quantity: {ele.quantity} unit: {ele.unit}</li>
+            
+            totalNutrientsarr.map(([key,value])=>{
+              return <ul><b>{key}</b> <li className='mx-4'>{value.label}</li> <li className='mx-4'>{value.quantity}</li> <li className='mx-4'>{value.unit}</li></ul>
             })}
             
             </ul>
-          </div> */}
+          </div>
           <div className="col-md-6">
             <ul>
               <h3> Health Labels</h3>
-            {recipe.healthLabels.map((ele)=>{
-              return <li> {ele}</li>
+            {recipe.healthLabels.map((ele,i)=>{
+              return <li key={i}> {ele}</li>
             })}
             </ul>
+          </div>
+          <div className='col'>
+            <h3>totalDaily</h3>
+          {totalDailyarr.map(([key,value])=>{
+            return <ul><b>{key}</b><li className='mx-3'>{value.label}</li><li className='mx-3'>{value.quantity}</li> <li className='mx-3'>{value.unit}</li></ul>
+          })}
+
+
           </div>
 
         </div>
