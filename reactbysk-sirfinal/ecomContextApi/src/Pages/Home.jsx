fdfc3eaff1d82a9d379725f086f8b705231dd52c
sakshi0303 <65711308+sakshi0303@users.noreply.github.com>
2024-09-21 18:10:49 +0530

    sdsd

--- reactbysk-sirfinal/ecomContextApi/src/Pages/Home.jsx
+++ reactbysk-sirfinal/ecomContextApi/src/Pages/Home.jsx
@@ -9,7 +9,20 @@
 // skelton, // npm i react-loading-skeleton
 
 // react-toastify to make alert box visually good
-import React,{useState , useEffect} from 'react'
+
+// pagination in bootstrap
+    // > starting indx , endingidx
+    // > const[currentpage,setcurrentpage]=useState(1)
+    // let number_of_items=10
+    // let lastindex=number_of_item*currentpage
+    // let firstindex=last_index-number_of_item
+    // let SlicedItem- arr.Slice(startindex-lastindex) (lastidx=10) therefore slieditem=(0-9)
+    // number of button: Math.ciel (arr.length/no_of_items)
+    // Array(10)..key()->[0,1,2,3,4,5,6,7,8,9]
+    // let buttonArry=[...Array(no_of_button).fill(0).keys()]
+    // [0-9]// 
+  
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Demo from '../Components/Demo';
 import { Link } from 'react-router-dom';
@@ -21,82 +34,97 @@ import { useContext } from 'react';
 import CartContext from '../Context/CartContext';
 
 const Home = () => {
-  let ctx=useContext(CartContext)
+  let ctx = useContext(CartContext)
   console.log(ctx);
-  
+
   const [product, setproduct] = useState([]);
   const [error, seterror] = useState(false);
   const [loading, setloading] = useState(false);
-  const getdata=async()=>{
+  const getdata = async () => {
     setloading(true)
-    try{
-    let res=await axios.get('https://dummyjson.com/products?limit=0&skip=0')
-    console.log("product",res);
-    setproduct(res.data.products)
-    setloading(false)
-    }catch(error){
+    let res = await axios.get('https://dummyjson.com/products?limit=0&skip=0')
+    try {
+      console.log("product", res);
+      setproduct(res.data.products)
+      setloading(false)
+    } catch (error) {
       console.log(error);
       seterror(true)
     }
   }
   useEffect(() => {
     getdata()
-    
+
   }, [])
 
-  
- 
-  
- 
+
+
+
+
   return (
     <div>
       {loading ? (
 
-      <div className='row m-0 p-0 justify-content-center gap-3'>
-         <SkeletonTheme baseColor="#202020" highlightColor="#444">
-        {Array(6).fill(0).map((ele,index)=>{
-          return <div className='col-md-3 m-4'>
-            <Skeleton height={200} />
-            <div className='d-flex justify-content-between'>
-            <Skeleton height={100} />
-            <Skeleton height={100} />
-            </div>
-            </div>
+        <div className='row m-0 p-0 justify-content-center gap-3'>
+          <SkeletonTheme baseColor="#202020" highlightColor="#444">
+            {Array(6).fill(0).map((ele, index) => {
+              return <div className='col-md-3 m-4'>
+                <Skeleton height={200} />
+                <div className='d-flex justify-content-between'>
+                  <Skeleton height={100} />
+                  <Skeleton height={100} />
+                </div>
+              </div>
 
-        })
-      }
-        </SkeletonTheme>
+            })
+            }
+          </SkeletonTheme>
 
-      </div>)
-      :
-      (
-      <div>
+        </div>)
+        :
+        (
+          <div>
 
-        { error===false?
-        (<div className='row m-0 p-0 bg-info justify-content-center gap-3'>
-        {
-        product.map((ele,i)=>{
-          return ele.thumbnail && <div key={i}className="card" style={{width: '18rem'}}>
-    <img src={ele.thumbnail} className="card-img-top" alt="..." />
-    <div className="card-body">
-      <h5 className="card-title">{ele.title}</h5>
-      <p className="card-text"><strong>Price</strong>{ele.price} $</p>
-      <Link to="/view" state={ele} className="btn btn-primary">View Details</Link>
-      <button className='btn btn-info mx-1' onClick={()=>ctx.AddtoCart(ele)} >Add to cart</button>
-    </div>
-  </div>
-  
-  
-        })}
-  
-  
-          
-      </div>) :
-      <Demo/>
+            {error === false ?
+              (<div className='row m-0 p-0 bg-info justify-content-center gap-3'>
+                {
+                  product.map((ele, i) => {
+                    return ele.thumbnail && <div key={i} className="card" style={{ width: '18rem' }}>
+                      <img src={ele.thumbnail} className="card-img-top" alt="..." />
+                      <div className="card-body">
+                        <h5 className="card-title">{ele.title}</h5>
+                        <p className="card-text"><strong>Price</strong>{ele.price} $</p>
+                        <Link to="/view" state={ele} className="btn btn-primary">View Details</Link>
+                        <button className='btn btn-info mx-1' onClick={() => ctx.AddtoCart(ele)} >Add to cart</button>
+                      </div>
+                    </div>
+
+
+                  })}
+
+
+
+              </div>) :
+              <Demo />
+            }
+          </div>
+        )
       }
-      </div>
-      )
-      }
+
+      <nav aria-label="Page navigation example">
+        <ul className="pagination justify-content-center">
+          <li className="page-item disabled">
+            <a className="page-link">Previous</a>
+          </li>
+          <li className="page-item"><a className="page-link" href="#">1</a></li>
+          <li className="page-item"><a className="page-link" href="#">2</a></li>
+          <li className="page-item"><a className="page-link" href="#">3</a></li>
+          <li className="page-item">
+            <a className="page-link" href="#">Next</a>
+          </li>
+        </ul>
+      </nav>
+
     </div>
   )
 }
