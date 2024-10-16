e34a43cfa516e9e02a219de585be30294c3423f9
sakshi0303 <65711308+sakshi0303@users.noreply.github.com>
2024-10-16 21:38:37 +0530

    df

--- twitterApp/frontend-old/src/pages/Pnf.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Pnf = () => {
-  return (
-    <div className='text-center bg-black'>Pnf 404</div>
-  )
-}
-
-export default Pnf
--- twitterApp/frontend-old/src/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import Userreducer from './UserSlice'
-
-export const store = configureStore({
-  reducer: {
-    user:Userreducer
-  },
-})
