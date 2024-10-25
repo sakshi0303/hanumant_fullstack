a34b24237783a76300750184725a83cf40335502
sakshi0303 <65711308+sakshi0303@users.noreply.github.com>
2024-10-25 16:00:50 +0530

    Sd

--- twitterApp/backend/server.js
+++ twitterApp/backend/server.js
@@ -11,7 +11,7 @@ app.set('view engine','ejs')
 const connecttodb=require('./db')
 connecttodb()
 
-app.use(express.json())
+app.use(express.json({limit:"50mb"}))
 app.use(cors()) //not blocking the api request sending from one port to another
 // isntall toash
 
