00078dae06dd56bf53febca47629d65b49f56166
sakshi0303 <65711308+sakshi0303@users.noreply.github.com>
2024-11-19 18:34:25 -0600

    sf

---
+++ springboot/SpringSecurityProject/src/main/java/com/SpringSecurity/SpringSecurityProject/MyController.java
@@ -0,0 +1,28 @@
+// package com.SpringSecurity.SpringSecurityProject;
+
+// import org.springframework.stereotype.Controller;
+// import org.springframework.web.bind.annotation.GetMapping;
+
+// @Controller
+// public class MyController {
+	
+// 	@GetMapping("/home")
+// 	public String home() {
+// 		return "home";
+// 	}
+	
+// 	@GetMapping("/admin/home")
+// 	public String handleAdmin() {
+// 		return "admin_home";
+// 	}
+	
+// 	@GetMapping("/user/home")
+// 	public String handleUser() {
+// 		return "user_home";
+// 	}
+	
+	
+	
+	
+
+// }
